4271148bf1c5b17c68967e728dd50699d81c9b17
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-11-19 16:57:43 +0100

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#define  OPEN_PIN 4
+#define  CLOSE_PIN 5
+#define  DOOR_PIN 9
+#define DOOR_PERIOD 150
+#define SCHEDULER_PERIOD 150
+#define L1_PIN 2
+#define L2_PIN 3
 #include <Arduino.h>
 #include "Wire.h"
 #include "Scheduler.h"
@@ -6,16 +13,18 @@
 Scheduler sched;
 Light* l1;
 Light* l2;
-
+bool emptying = false;
+bool crytTemp = false;
+bool full = false;
 
 void setup() {
   Serial.begin(9600);
-  sched.init(150);
-  l1 = new Led(2);
-  l2 = new Led(3);
+  sched.init(SCHEDULER_PERIOD);
+  l1 = new Led(L1_PIN);
+  l2 = new Led(L2_PIN);
   //open sul pin 4, close sul 5
-  Task* doorTask = new DoorTask(l1, l2, 9, 4, 5);
-  doorTask->init(150);
+  Task* doorTask = new DoorTask(l1, l2, DOOR_PIN, OPEN_PIN, CLOSE_PIN);
+  doorTask->init(DOOR_PERIOD);
   sched.addTask(doorTask);
 }
 

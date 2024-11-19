80aefe4ac7801143ff1e49774d6ea77fccba02f2
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-11-19 17:55:56 +0100

    modified containerTask

--- src/ContainerTask.cpp
+++ src/ContainerTask.cpp
@@ -1,7 +1,7 @@
 #include "ContainerTask.h"
 #include <Arduino.h>
 #include "Sonar.h"
-
+#include "Light.h"
 extern bool emptying;
 extern bool crytTemp;
 extern bool full;
@@ -12,4 +12,59 @@ ContainerTask::ContainerTask(Light *l1, Light *l2, int echoPin, int trigPin)
   this->l2 = l2;
   this->sonar = new Sonar(trigPin, echoPin);
   this->containerState = NOT_FULL;
+}
+void ContainerTask::tick()
+{
+  switch (this->containerState)
+  {
+  case NOT_FULL:
+    {
+      Serial.println("not full");
+      float distance = sonar->getDistance();
+      Serial.println(distance);
+      if (distance < MIN_DISTANCE)
+      {
+        distance = MIN_DISTANCE;
+      }
+      else if (distance > MAX_DISTANCE)
+      {
+        distance = MAX_DISTANCE;
+      }
+      float percentage = 100 - (distance - MIN_DISTANCE) / (MAX_DISTANCE - MIN_DISTANCE) * 100;
+      Serial.println(percentage);
+      /*TODO mandare percentuale da visualizzare*/
+      if (distance <= MIN_DISTANCE)
+      {
+        full = true;
+        l1->switchOff();
+        l2->switchOn();
+        this->containerState = FULL;
+      }
+      break;
+    }
+    case FULL:
+    {
+      Serial.println("full");
+      bool pressed= true; /*TODO ricevere messaggio da GUI*/
+      if (pressed)
+      {
+        emptying = true;
+        this->containerState = EMPTYING;
+        startEmptying = millis();
+      }
+      break;
+    }
+    case EMPTYING:
+    {
+      Serial.println("emptying");
+      if (millis() - startEmptying > T3)
+      {
+        emptying = false;
+        this->containerState = NOT_FULL;
+        l1->switchOn();
+        l2->switchOff();
+      }
+      break;
+    }
+  }
 }
--- src/ContainerTask.h
+++ src/ContainerTask.h
@@ -1,5 +1,8 @@
 #ifndef __CONTAINERTASK__
 #define __CONTAINERTASK__
+#define MAX_DISTANCE 1
+#define MIN_DISTANCE 0.1
+#define T3 5000
 #include "Task.h"
 #include "Light.h"
 #include "DistanceDevice.h"
@@ -10,6 +13,7 @@ class ContainerTask : public Task
   Light* l1;
   Light* l2;
   DistanceDevice* sonar;
+  long startEmptying;
   enum {FULL, NOT_FULL,EMPTYING} containerState;
 public:
   ContainerTask(Light* l1, Light* l2,int echoPin, int trigPin);

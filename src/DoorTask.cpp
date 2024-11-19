69f9edd85469e99e7ea8aa64564424ca16d87495
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-11-19 16:54:25 +0100

    update doorTask

--- src/DoorTask.cpp
+++ src/DoorTask.cpp
@@ -1,59 +1,115 @@
 #include "DoorTask.h"
 #include <Arduino.h>
 
-DoorTask::DoorTask(Light* l1, Light* l2, int doorPin, int openPin, int closePin)
+extern bool emptying;
+extern bool crytTemp;
+extern bool full;
+
+DoorTask::DoorTask(Light *l1, Light *l2, int doorPin, int openPin, int closePin)
 {
   this->l1 = l1;
   this->l2 = l2;
   this->door = new ServoMotorImpl(doorPin);
   this->openButton = new ButtonImpl(openPin);
   this->closeButton = new ButtonImpl(closePin);
-  
+
   this->doorState = CLOSED;
   l1->switchOn();
   l2->switchOff();
   Serial.println("Inizializzazione fatta");
   /*door->on();
   door->setPosition(90);
-  delay(100);*/
+  delay(100);
+  door->off();*/
 }
 
 void DoorTask::tick()
 {
   Serial.println("stato: " + String(this->doorState));
-        switch (doorState) {
-        case OPENED: {
-          Serial.println("Check del closed");
-          bool closePressed = closeButton->isPressed();
-          if(closePressed || millis() - lastOpen > 5000){
-            doorState = CLOSED;
-            /*door->on();
-            door->setPosition(90);
-            delay(1000);
-            door->off();*/
-            Serial.println("chiudi porta");
-          }
-          break;
-        }
-          
-        case CLOSED: {
-          Serial.println("Check del open");
-          bool openPressed = openButton->isPressed();
-          if(openPressed){
-              lastOpen=millis();
-              doorState = OPENED;
-              /*door->on();
-              door->setPosition(180);
-              door->off();*/
-              Serial.println("apri porta");
-          }            
-          break;
-        }
-            
-        case REVERSED: {
-          Serial.println("Caso REVERSED");
-          break;
-        }
+  switch (doorState)
+  {
+  case OPENED:
+  {
+
+    long elapsed = millis() - lastOpen;
+    Serial.println("elapsed: " + String(elapsed));
+    Serial.println("Check del closed");
+    bool closePressed = closeButton->isPressed();
+    if (closePressed || elapsed > T1)
+    {
+      doorState = CLOSED;
+      moveDoor(CLOSED_ANGLE);
+      Serial.println("chiudi porta");
     }
+
+    if (full || crytTemp)
+    {
+      doorState = BLOCKED;
+      moveDoor(CLOSED_ANGLE);
+      Serial.println("porta bloccata");
+    }
+    break;
+  }
+
+  case CLOSED:
+  {
+    Serial.println("Check del open");
+    bool openPressed = openButton->isPressed();
+    if (openPressed)
+    {
+      lastOpen = millis();
+      doorState = OPENED;
+      moveDoor(OPENED_ANGLE);
+      Serial.println("apri porta");
+    }
+
+    if (full || crytTemp)
+    {
+      doorState = BLOCKED;
+      moveDoor(CLOSED_ANGLE);
+      Serial.println("porta bloccata");
+    }
+
+    if (emptying)
+    {
+      doorState = REVERSED;
+      moveDoor(REVERSED_ANGLE);
+      Serial.println("porta invertita");
+    }
+    break;
+  }
+
+  case REVERSED:
+  {
+    Serial.println("Caso REVERSED");
+    if (!emptying)
+    {
+      doorState = CLOSED;
+      moveDoor(CLOSED_ANGLE);
+      Serial.println("porta chiusa");
+    }
+    break;
+  }
+
+  case BLOCKED:
+  {
+    Serial.println("Caso BLOCKED");
+    if (!full && !crytTemp)
+    {
+      doorState = CLOSED;
+      moveDoor(CLOSED_ANGLE);
+      Serial.println("porta chiusa");
+    }
+    break;
+  }
     Serial.println("dopo switch: " + String(doorState));
-    }
+  }
+}
+
+void DoorTask::moveDoor(int angle)
+{
+  /*door->on();
+  door->setPosition(angle);
+  delay(1000);
+  door->off();*/
+}
--- src/DoorTask.h
+++ src/DoorTask.h
@@ -1,6 +1,11 @@
 #ifndef __DOORTASK__
 #define __DOORTASK__
 
+#define T1 5000
+#define CLOSED_ANGLE 90
+#define OPENED_ANGLE 180
+#define REVERSED_ANGLE 0
+
 #include "Task.h"
 #include "Light.h"
 #include "ButtonImpl.h"
@@ -12,12 +17,13 @@ class DoorTask : public Task
   int lastOpen;
   Light* l1;
   Light* l2;
-  enum {OPENED, CLOSED,REVERSED} doorState;
+  enum {OPENED, CLOSED,REVERSED, BLOCKED} doorState;
   Button* openButton;
   Button* closeButton;
   ServoMotor* door;
 public:
   DoorTask(Light* l1, Light* l2, int doorPin, int openPin, int closePin);
   void tick();
+  void moveDoor(int angle);
 };
 #endif

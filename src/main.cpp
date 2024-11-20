ba0c48445e60a85dc700dac008bdc724d5e81c5d
Alex Frisoni <alex.frisoni2@studio.unibo.it>
2024-11-20 11:50:55 +0100

    modified main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #define  OPEN_PIN 4
 #define  CLOSE_PIN 5
 #define  DOOR_PIN 9
+#define PIR_PIN 6
+#define USERDETECTION_PERIOD 100
 #define DOOR_PERIOD 150
-#define SCHEDULER_PERIOD 100
+#define SCHEDULER_PERIOD 1000
 #define L1_PIN 2
 #define L2_PIN 3
 #define TRIG_PIN 13
@@ -14,6 +16,7 @@
 #include "Led.h"
 #include "DoorTask.h"
 #include "ContainerTask.h"
+#include "UserDetectionTask.h"
 Scheduler sched;
 Light* l1;
 Light* l2;
@@ -30,10 +33,13 @@ void setup() {
   Task* doorTask = new DoorTask(l1, l2, DOOR_PIN, OPEN_PIN, CLOSE_PIN);
   //trig sul pin 13, echo sul 12
   Task* containerTask= new ContainerTask(l1, l2, ECHO_PIN, TRIG_PIN); 
+  Task* userDetectionTask = new UserDetectionTask(PIR_PIN);
+  userDetectionTask->init(USERDETECTION_PERIOD);
   doorTask->init(DOOR_PERIOD);
   containerTask->init(CONTAINER_PERIOD);
   sched.addTask(doorTask);
   sched.addTask(containerTask);
+  sched.addTask(userDetectionTask);
 }
 
 void loop() {
